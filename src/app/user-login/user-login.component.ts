766e551fb1a55a069eee4bcb9c7d0157057ab674
PranavJadhav011 <01fe20bcs099@kletech.ac.in>
2022-12-07 07:40:50 +0530

    initial commit

---
+++ src/app/user-login/user-login.component.ts
@@ -0,0 +1,60 @@
+import { Component,NgZone, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-user-login',
+  templateUrl: './user-login.component.html',
+  styleUrls: ['./user-login.component.css']
+})
+export class UserLoginComponent implements OnInit {
+
+  signupUsers: any[] = [];
+  isUserExist: boolean = false;
+
+  signupObj:any = {
+    userName: '',
+    email: '',
+    password: ''
+  };
+
+  loginObj: any = {
+    userName: '',
+    password: ''
+  };
+
+  constructor(
+    private router:Router,
+    private ngZone:NgZone
+  ) { }
+
+  ngOnInit(): void {
+    const localData = localStorage.getItem('signUpUsers');
+    if(localData != null) {
+      this.signupUsers = JSON.parse(localData);
+    }
+  }
+
+  onSignUp() {
+    this.signupUsers.push(this.signupObj);
+    localStorage.setItem('signUpUsers',JSON.stringify(this.signupUsers));
+    this.signupObj = {
+      userName: '',
+      email: '',
+      password: ''
+    };
+    console.log(this.signupObj.email+" "+this.signupObj.password);
+  }
+
+  onLogin() {
+    this.isUserExist = this.signupUsers.find(m => m.email == this.loginObj.userName
+      && m.password == this.loginObj.password);
+
+      if(this.isUserExist != undefined) {
+        // alert("login");
+        this.ngZone.run(()=> this.router.navigateByUrl('/books-list'))
+      } else {
+        alert('Wrong Credentials Entered');
+      }
+  }
+
+}
